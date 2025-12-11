7cf16f7f66267011cad396013acf9a9dbdbb9763
Mohit-Sharma-2614 <mohitsharma921624@gmail.com>
2025-12-11 22:34:36 +0530

    Learned to use the basic shapes like drawLine, drawArc, drawCircle, drawRect etc.. Also learned about the DrawScope ( size, center), stroke, Size(), Offset() etc.

--- app/src/main/java/com/example/composeexamples/Examples.kt
+++ app/src/main/java/com/example/composeexamples/Examples.kt
@@ -2,17 +2,23 @@ package com.example.composeexamples
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -82,9 +88,84 @@ fun CanvasExamples(){
 
 }
 
+@Composable
+fun MyCanvas2(){
+    Box(
+        modifier = Modifier
+            .size(height = 400.dp, width = 400.dp)
+            .drawBehind{
+                drawArc(
+                    color = Color.Red,
+                    startAngle = 0f,
+                    sweepAngle = 90f,
+                    useCenter = true,
+                    style = Stroke(width = 3.dp.toPx())
+                )
+            }
+    )
+}
+
+@Composable
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp)
+    ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(
+                100f,
+                100f
+            ),
+            size = Size(
+                100f,
+                100f
+            ),
+            style = Stroke(
+                width = 3.dp.toPx(),
+            )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red, Color.Yellow),
+                center = center,
+                radius = 60.dp.toPx()
+            ),
+            radius = 70.dp.toPx(),
+            center = center
+        )
+        drawArc(
+            color = Color.Green,
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = true, // Try false here and it will show that the edge of ark is connected directly
+            topLeft = Offset(100f,500f),
+            size = Size(200f,200f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(500f,100f),
+            size = Size(200f, 300f),
+        )
+        drawLine(
+            color = Color.Cyan,
+            start = Offset(300f,700f),
+            end = Offset(700f,700f),
+            strokeWidth = 5.dp.toPx()
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewCanvas(){
-    CanvasExamples()
+    MyCanvas()
 }
